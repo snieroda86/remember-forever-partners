9266d051986249b5bb22834f3640a903bfb68268
Sebastian Nieroda <snieroda86@gmail.com>
2025-05-16 20:09:09 +0200

    Prypisywanie tagów w panelu admia dla usera ogarniete

--- views/business-partner-single.php
+++ views/business-partner-single.php
@@ -6,6 +6,35 @@ if (!$user) {
     return;
 }
 
+// Enabled languages
+global $sitepress;
+
+if ( isset( $sitepress ) ) {
+    $enabled_languages = $sitepress->get_active_languages();
+    
+   
+} else {
+    echo 'WPML nie jest dostępny.';
+}
+
+// Currnet lang code 
+$current_lang_code = apply_filters( 'wpml_current_language', null );
+
+
+// Get tags ids from all languages
+
+$current_lang_product_tags = get_terms( array(
+    'taxonomy' => 'product_tag',
+    'hide_empty' => true,
+) );
+
+// if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+//     foreach ( $terms as $term ) {
+//         echo $term->term_id.' , '.'<br>';
+//     }
+// }
+
+
 
 $company_name = get_user_meta($user_id, 'company_name', true);
 $company_nip = get_user_meta($user_id, 'company_nip', true);
@@ -18,9 +47,14 @@ $partner_percentage_discount = get_user_meta($user_id, 'partner_percentage_disco
 if (isset($_POST['update_partner']) && check_admin_referer('update_partner_data', 'update_partner_nonce')) {
     $new_access = isset($_POST['partner_has_access']) ? (int) $_POST['partner_has_access'] : 0;
     $new_discount = isset($_POST['partner_percentage_discount']) ? (int) $_POST['partner_percentage_discount'] : 0;
+    $partner_p_tags_assigned = isset($_POST['partner_p_tags_assigned']) ?  $_POST['partner_p_tags_assigned'] : null;
 
     update_user_meta($user->ID, 'partner_has_access', $new_access);
     update_user_meta($user->ID, 'partner_percentage_discount', $new_discount);
+
+    if(!empty($partner_p_tags_assigned) && !is_null($partner_p_tags_assigned) && !empty($current_lang_code)){
+        update_user_meta($user->ID, $current_lang_code.'_partner_p_tags_assigned', $partner_p_tags_assigned);
+    }
 
     echo '<div class="updated notice"><p>Dane zostały zaktualizowane.</p></div>';
 
@@ -89,6 +123,55 @@ if (isset($_POST['update_partner']) && check_admin_referer('update_partner_data'
                     <input type="number" name="partner_percentage_discount" id="partner_percentage_discount"
                            value="<?php echo esc_attr($partner_percentage_discount); ?>" min="0" max="100"> %
                 </p>
+
+                <!-- Tags -->
+                <p>
+                    <label for="partner_has_access">Przypisz TAGI:</label><br>
+                    <select multiple name="partner_p_tags_assigned[]" id="partner_p_tags_assigned">
+                        <option value=""  selected disabled>--Wybierz tagi--</option>
+                        <?php if(!empty($current_lang_product_tags)): ?>
+                            <?php foreach ($current_lang_product_tags as $tag) { ?>
+                                <option value="<?php echo $tag->term_id ?>"><?php echo $tag->name ?></option>
+                            <?php } ?>
+                        <?php endif; ?>
+                    </select>
+                </p>
+
+
+                <!-- Assigned tags -->
+                <table class="widefat striped">
+                    <?php if($enabled_languages): ?>
+                    <thead style="background: #eee;">
+                        <tr>
+                            <th>Język</th>
+                            <td>Przypisane tagi</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($enabled_languages as $enable_lang): ?>
+                        <tr>
+                            <th><?php echo $enable_lang['display_name'] ?></th>
+                            <td>
+                                <?php 
+                                $_partner_p_tags_assigned = get_user_meta($user_id, $enable_lang['code'] . '_partner_p_tags_assigned', true);
+
+                                if (!empty($_partner_p_tags_assigned) && is_array($_partner_p_tags_assigned)) {
+                                    foreach ($_partner_p_tags_assigned as $tag_id) {
+                                        if (is_numeric($tag_id)) {
+                                            $lang_tag = get_term((int) $tag_id, 'product_tag');
+                                            if ($lang_tag && !is_wp_error($lang_tag)) {
+                                                echo '<span style="background: blue; color:#fff;border-radius:3px;padding:3px 8px;margin-right:4px">' . esc_html($lang_tag->name) . '</span>';
+                                            }
+                                        }
+                                    }
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                    <?php endif; ?>
+                </table>
 
                 <p>
                     <input type="submit" name="update_partner" class="button button-primary" value="Zapisz zmiany">
